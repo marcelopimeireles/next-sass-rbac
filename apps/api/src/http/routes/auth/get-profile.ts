cf8c264b1480bb10e089a9b5999ccfe01efa84a4
Marcelo Meireles <marcelopimeireles@gmail.com>
2024-05-23 13:20:40 +0100

    auth with password

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function authenticateWithPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/profile',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authenticated user profile',
+        response: {
+          200: z.object({
+            id: z.string().uuid(),
+            name: z.string(),
+            email: z.string().email(),
+            avatarUrl: z.string().url(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { sub } = await request.jwtVerify<{ sub: string }>()
+
+      const user = await prisma.user.findUnique({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatarUrl: true,
+        },
+        where: {
+          id: sub,
+        },
+      })
+      return reply.send({ user })
+    },
+  )
+}
